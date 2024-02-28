60fa33d2d48408bd2024855351d501f9bfef2ee2
Jumaga11 <Tecnolojuancho@live.com>
2024-02-27 20:32:23 -0500

    archivo visibility.php finalizado

--- 01-oop/03-visibility.php
+++ 01-oop/03-visibility.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,7 +18,7 @@
 
             h2 {
                 margin: 0;
-            } 
+            }
 
             form {
                 border: 2px solid #fff6;
@@ -28,7 +29,7 @@
                 padding: 10px;
                 width: 300px;
 
-                label {
+                label0 {
                     display: flex;
                     justify-content: space-between;
                     gap: 1.4rem;
@@ -53,70 +54,78 @@
         }
     </style>
 </head>
+
 <body>
     <nav class="controls">
         <a href="index.html">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM231 127c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-71 71L376 232c13.3 0 24 10.7 24 24s-10.7 24-24 24l-182.1 0 71 71c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L119 273c-9.4-9.4-9.4-24.6 0-33.9L231 127z"/></svg>    
+            <svg xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                <path fill="#ffffff"
+                    d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM231 127c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-71 71L376 232c13.3 0 24 10.7 24 24s-10.7 24-24 24l-182.1 0 71 71c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L119 273c-9.4-9.4-9.4-24.6 0-33.9L231 127z" />
+            </svg>
         </a>
     </nav>
     <main>
         <h1>03- Visibility</h1>
         <section>
-            <?php
-                class TableMaker {
-                    // Attributes
-                    private $nr; //Number of Rows
-                    private $nc; //Number od Columns
-                    // Methods
-                    public function __construct($nr, $nc) {
-                        $this->nr = $nr;
-                        $this->nc = $nc;
-                    }
-                    public function drawTable() {
-                        echo $this->startTable();
-                        echo $this->contentTable();
-                        echo $this->endTable();
-                    }
-                    private function startTable() {
-                        return '<table>';
-                    }
-                    private function contentTable() {
-                        $content = '';
-                        for($i = 0 ; $this -> nr ; $i ++){
-                            $content .= '<tr>';
-                            for($j = 0 ; $j < $this -> nc ; $j++){
-                                $content .= '<td></td>';
-                            }  
-                            $content .= '</tr>';
-                        }
-                        return $content;
-                    }
-                    private function endTable() {
-                        return '</table>';
-                    }
-                }
-                if($_SERVER["REQUEST_METHOD"]=="POST"){
-                    $nr = $_POST ['nr'];    
-                    $nc = $_POST ['nc'];
-                    $table = new TableMaker($nr, $nc);
-                    $table->drawTable();
-                }  
-            ?>
             <h2>Table Maker</h2>
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <label>
-                <p>Rows:</p>
-                <input type="range" name="nr" min="1" max="20" step="1" value="1" oninput="o1.value=this.value">
-                <output id="o1">1</output>
-            </label>
-            <label>
-                <p>Columns:</p>
-                <input type="range" name="nc" min="1" max="20" step="1" value="1" oninput="o2.value=this.value">
-                <output id="o2">1</output>
-            </label>
-            <button type="submit"> Make Table </button>
-        </form>
+            <form action="" method="post">
+                <label>
+                    <p>Rows:</p>
+                    <input type="range" name="nr" min="1" max="20" step="1" value="1" oninput="o1.value=this.value">
+                    <output id="o1">1</output>
+                </label>
+                <label>
+                    <p>Columns:</p>
+                    <input type="range" name="nc" min="1" max="20" step="1" value="1" oninput="o2.value=this.value">
+                    <output id="o2">1</output>
+                </label>
+                <button> Make Table </button>
+            </form>
+            <?php            
+            class TableMaker {
+                // Attributes
+                private $nr; //Number of Rows
+                private $nc; //Number od Columns
+            
+                // Methods
+                public function __construct() {
+                    if(isset($_POST['nr']) && isset($_POST['nc'])) {
+                        $this->nr = (int)$_POST['nr'];
+                        $this->nc = (int)$_POST['nc'];
+                    } else {                        
+                        $this->nr = 10;
+                        $this->nc = 8;
+                    }
+                }            
+                public function drawTable() {
+                    echo $this->startTable();
+                    echo $this->contentTable();
+                    echo $this->endTable();
+                }
+                private function startTable() {
+                    return '<table>';
+                }
+                private function contentTable() {
+                    $content = '';
+                    for ($i = 0; $i < $this->nr; $i++) {
+                        $content .= '<tr>';
+                        for ($j = 0; $j < $this->nc; $j++) {
+                            $content .= '<td></td>';
+                        }
+                        $content .= '</tr>';
+                    }
+                    return $content;
+                }            
+                private function endTable() {
+                    return '</table>';
+                }
+            }
+            $table = new TableMaker(); 
+            $table->drawTable();
+            ?>
         </section>
     </main>
 </body>
+
 </html>
