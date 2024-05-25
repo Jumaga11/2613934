846e6ec64e5d6f984b56fb008196620cb69fdf82
Jumaga11 <Tecnolojuancho@live.com>
2024-05-25 15:52:16 -0500

    Login, Register, Dashboard files created and finished succesfully !

---
+++ 07-layout/JS/scripts.js
@@ -0,0 +1,23 @@
+/*-------------------------------------------------------------
+-                     MENU CARRUSEL WELCOME                   -
+--------------------------------------------------------------*/ 
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        responsive: {
+            0: {
+                items: 1
+            }
+        }
+    })
+})
+
+/*-------------------------------------------------------------
+-                CARRUSEL DE ARCHIVO CATALOGO                 -
+--------------------------------------------------------------*/ 
+
